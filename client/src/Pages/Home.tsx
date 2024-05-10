4598b7a240a91b622762b4c91eba8fb26865640e
newrocker2468 <newrocker2468@gmail.com>
2024-05-10 21:48:14 +0530

    res cookie chaltat

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 import Cardcomp from "@/components/Cardcomp";
 import SideBar from "@/components/SideBar";
 import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
+
 
 export default function Home() {
   const [isSidebarVisible, setIsSidebarVisible] = useState(
@@ -16,21 +16,7 @@ export default function Home() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token");
-    const refreshToken = urlParams.get("refreshToken");
 
-    if (!token || !refreshToken) return;
-
-    const currentToken = Cookies.get("token");
-    const currentRefreshToken = Cookies.get("refreshToken");
-
-    if (currentToken || currentRefreshToken) return;
-
-    Cookies.set("token", token);
-    Cookies.set("refreshToken", refreshToken);
-  }, []);
 
   return (
     <div className='flex'>
