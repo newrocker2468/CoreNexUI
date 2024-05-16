d301a96a3fee07bedda42e770e03e7a973b43c7e
newrocker2468 <newrocker2468@gmail.com>
2024-05-16 14:19:03 +0530

    changed not found page

--- client/src/Pages/NotFoundPage.tsx
+++ client/src/Pages/NotFoundPage.tsx
@@ -1,15 +1,31 @@
 import "@/Styles/NotFoundPage.css";
+import { useNavigate } from "react-router-dom";
+import {toast} from "sonner"
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+  toast.error("Error 404! Page Not Found. Redirecting to Home Page", {
+    position: "top-center",
+      action:{
+        label:"X",
+        onClick:()=>toast.dismiss(),
+      }
+  });
+ 
+ 
+  setTimeout(() => {
+    navigate("/home");
+  }, 4000);
   return (
-<div className="container">
-  <form className="four-oh-four-form">
-    <input type="text" className="404-input"/>
-  </form>
+    <div className='container'>
+      <form className='four-oh-four-form'>
+        <input type='text' className='404-input' />
+      </form>
 
-  <div className="terminal">
-      <p className="prompt">The page you requested cannot be found right meow. Try typing 'kittens'.</p>
-      <p className="prompt output new-output"></p>
-  </div>
-</div>
+      <div className='terminal'>
+        <p className='prompt'>Error 404!</p>
+        <p className='prompt'>Page Not Found</p>
+        <p className='prompt output new-output'></p>
+      </div>
+    </div>
   );
 }
