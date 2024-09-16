b2d011dff955c91f3655b71e8f6b8a50c41c4643
newrocker2468 <newrocker2468@gmail.com>
2024-09-16 18:33:17 +0530

    Refactor server/index.js to update allowed origins

--- server/index.js
+++ server/index.js
@@ -44,9 +44,8 @@ app.use(
     origin: [
       "http://localhost:5173",
       `${process.env.FRONTEND_URL}`,
-      
       "https://firebasestorage.googleapis.com",
-      "https://corenexui.jaskaran.dev",
+      "https://corenexui.jaskaran.dev/",
       "https://core-nex-ui.vercel.app/",
       "https://core-nex-ui-s2jo.vercel.app/",
       "https://corenexui.netlify.app",
