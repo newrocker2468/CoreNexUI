7421f577a09f9afb113933ed36b4b37694d3897a
newrocker2468 <newrocker2468@gmail.com>
2025-05-13 20:19:47 +1000

    Update server port from 3000 to 3001 and add package-lock.json

--- server/index.js
+++ server/index.js
@@ -2328,8 +2328,8 @@ app.get("/files", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(3001, () => {
+  console.log("Server is running on port 3001.");
 });
 app.use((req, res, next) => {
   res.status(404).send("404 Not Found");
